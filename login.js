a72a55e0ad36633583782f0cb130506dcf830fd9
impotantsnotesforstudents-ux <impotantsnotesforstudents@gmail.com>
2026-06-30 23:49:33 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,18 +1,24 @@
+import { auth } from "./firebase.js";
+
 import {
     signInWithEmailAndPassword,
     GoogleAuthProvider,
     signInWithPopup
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
+// Email Login
+
 const loginForm = document.getElementById("loginForm");
 
 loginForm.addEventListener("submit", async (e) => {
+
     e.preventDefault();
 
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
     try {
+
         await signInWithEmailAndPassword(auth, email, password);
 
         alert("Login Successful!");
@@ -24,4 +30,29 @@ loginForm.addEventListener("submit", async (e) => {
         alert(error.message);
 
     }
+
+});
+
+// Google Login
+
+const googleBtn = document.getElementById("googleLogin");
+
+const provider = new GoogleAuthProvider();
+
+googleBtn.addEventListener("click", async () => {
+
+    try {
+
+        await signInWithPopup(auth, provider);
+
+        alert("Google Login Successful!");
+
+        window.location.href = "../index.html";
+
+    } catch (error) {
+
+        alert(error.message);
+
+    }
+
 });

24b9856fea9ef6bb6fb7542e29661ad5d2791b27
impotantsnotesforstudents-ux <impotantsnotesforstudents@gmail.com>
2026-06-30 23:13:12 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,1 +1,25 @@
+import { auth } from "./firebase.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
+const signupForm = document.getElementById("signupForm");
+
+signupForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+
+    if (password !== confirmPassword) {
+        alert("Passwords do not match!");
+        return;
+    }
+
+    try {
+        await createUserWithEmailAndPassword(auth, email, password);
+        alert("Account created successfully!");
+        window.location.href = "login.html";
+    } catch (error) {
+        alert(error.message);
+    }
+});

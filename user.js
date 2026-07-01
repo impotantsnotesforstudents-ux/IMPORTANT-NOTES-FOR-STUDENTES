0a5111883a897dde6b1b68d9a671fa40b1e9892a
impotantsnotesforstudents-ux <impotantsnotesforstudents@gmail.com>
2026-07-01 19:41:31 +0530

    Update user.js

--- user.js
+++ user.js
@@ -1,1 +1,40 @@
+import {
+    auth,
+    onAuthStateChanged,
+    signOut
+} from "./firebase.js";
 
+const userArea = document.getElementById("userArea");
+
+onAuthStateChanged(auth, (user) => {
+
+    if (user) {
+
+        const name = user.displayName || user.email;
+        const photo = user.photoURL || "images/LOGO.jpeg";
+
+        userArea.innerHTML = `
+            <div class="user-profile">
+                <img src="${photo}" class="profile-pic">
+                <span>${name}</span>
+                <button id="logoutBtn">Logout</button>
+            </div>
+        `;
+
+        document.getElementById("logoutBtn").addEventListener("click", async () => {
+
+            await signOut(auth);
+
+            window.location.reload();
+
+        });
+
+    } else {
+
+        userArea.innerHTML = `
+            <a href="pages/login.html" id="loginLink">Login</a>
+        `;
+
+    }
+
+});

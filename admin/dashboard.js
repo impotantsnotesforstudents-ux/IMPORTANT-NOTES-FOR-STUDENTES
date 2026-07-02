2ebacbdf8cd1ed0c4adc6a8722de7384eaf68e5a
impotantsnotesforstudents-ux <impotantsnotesforstudents@gmail.com>
2026-07-02 19:25:31 +0530

    Update dashboard.js

--- admin/dashboard.js
+++ admin/dashboard.js
@@ -1,2 +1,33 @@
-document.getElementById("usersCount").innerText = "Loading...";
-document.getElementById("premiumCount").innerText = "Loading...";
+import { db } from "../firebase.js";
+
+import {
+    collection,
+    getDocs
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const usersCount = document.getElementById("usersCount");
+const premiumCount = document.getElementById("premiumCount");
+
+async function loadDashboard() {
+
+    const snapshot = await getDocs(collection(db, "users"));
+
+    let totalUsers = 0;
+    let premiumUsers = 0;
+
+    snapshot.forEach((doc) => {
+
+        totalUsers++;
+
+        if (doc.data().premium === true) {
+            premiumUsers++;
+        }
+
+    });
+
+    usersCount.innerText = totalUsers;
+    premiumCount.innerText = premiumUsers;
+
+}
+
+loadDashboard();
